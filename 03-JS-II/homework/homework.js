3b469137394cd45c7f5eedd08316e55778fdfcb5
marceloaguierre50 <marceloaguierre50@gmail.com>
2022-11-11 20:32:02 -0300

    nuevo archivo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,14 +5,29 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if ( x > y){
+    return x
+  }
+  else if (y > x){
+    return y
+  }
+  else if ( y === x){
+    return y
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18){
+    return "Allowed"
+  }
+  else if ( edad < 18){
+    return "Not allowed"
+  }
 }
-  
+
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
