382ca9c8091c408fde617b6dfb8a2f98c2921fc8
marceloaguierre50 <marceloaguierre@github.com>
2023-04-10 22:00:46 -0300

    archivo modificado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -201,19 +201,29 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if(numero > 0) {
+    return "Es positivo";
+  }
+  else if ( numero < 0) {
+    return "Es negativo";
+  }
+  else
+  return false
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+return str + "!"
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var combinarNombres = nombre + " " + apellido ;
+  return combinarNombres
   
 }
 
