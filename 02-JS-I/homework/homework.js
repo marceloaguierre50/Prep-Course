eab0bf684e0a30fb831e6655d56b2f636cac6051
marceloaguierre50 <marceloaguierre@github.com>
2023-04-12 21:20:54 -0300

    archivo modificado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -231,26 +231,30 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return "Hola" + " " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  obtenerAreaRectangulo = ancho * alto
+  return obtenerAreaRectangulo
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  retornarPerimetro = lado + lado + lado + lado
+  return retornarPerimetro
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  areaDelTriangulo = base * altura / 2
+  return areaDelTriangulo
 
 }
 
@@ -259,7 +263,10 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  let dolar = 1.20
+  deEuroAdolar = euro * dolar
+  return deEuroAdolar
+
 }
 
 
@@ -269,7 +276,6 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
 }
 
 
